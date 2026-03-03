5a6d5503e3dd5e90d9f05963bee99c531b223d00
Ben <185095765+ben854719@users.noreply.github.com>
2026-03-03 14:08:51 -0500

    File to run AWS Cloud

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,8 @@
+import json
+
 def lambda_handler(event, context):
-    # the logic here
-    return {"statusCode": 200, "body": "OK"}
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
